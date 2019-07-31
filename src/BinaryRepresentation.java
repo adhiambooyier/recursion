16252b6a7ac25424787df8754492e481dc0ae56d
adhiambo peres <adhiambo@sendyit.com>
2019-07-31 11:25:39 +0300

    combinations s/o exception

--- src/BinaryRepresentation.java
+++ src/BinaryRepresentation.java
@@ -1,5 +1,33 @@
-public class BinaryRepresentation {
-    public static void main (String[] args){
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class BinaryRepresentation {
+    /*Binary representation. Write a program that takes a positive integer n (in
+decimal) as a command-line argument and prints its binary representation. Use
+the following simpler method: repeatedly divide 2 into n and read the remainders
+backward. First, write a while loop to carry out this computation and print the bits
+in the wrong order. Then, use recursion to print the bits in the correct order.*/
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number n: ");
+        int n = input.nextInt();
+        List<Integer> wrongOrderBits = new ArrayList<>();
+
+        while (n >= 1) {
+            int quotient = n / 2;
+            int remainder = n % 2;
+            wrongOrderBits.add(remainder);
+            n = quotient;
+        }
+        System.out.println(wrongOrderBits);
+        print(wrongOrderBits, wrongOrderBits.size() - 1);
+    }
+
+    private static void print(List<Integer> bits, int index) {
+        if (index < bits.size() && index >= 0) {
+            System.out.print(bits.get(index));
+            print(bits, --index);
+        }
     }
 }
--- src/Combinations.java
+++ src/Combinations.java
@@ -1,5 +1,36 @@
-public class Combinations {
-    public static void main (String[] args){
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Combinations {
+    /*
+    Combinations. Write a program Combinations that takes an integer command-line argument n
+    and prints all 2 n combinations of any size. A combination is
+    a subset of the n elements, independent of order. As an example, when n = 3, you
+    should get the following output:
+    a ab abc ac b bc c
+    Note that your program needs to print the empty string (subset of size 0).
+     */
+
+    public static void main(String[] args) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number n: ");
+        int n = input.nextInt();
+        List<String> lettersToCombine = new ArrayList<>();
+        while (n >= 1) {
+            String letter = Character.toString(alphabet.charAt(n - 1));
+            lettersToCombine.add(letter);
+            n = n - 1;
+        }
+        System.out.println(lettersToCombine.toString());
+        combine(lettersToCombine, n);
+    }
+
+    private static void combine(List<String> letters, int n) {
+        if (n == 1){
+            System.out.println(letters.get(n));
+        }
+        combine(letters, --n);
     }
 }
